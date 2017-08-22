5a0f061fda8c0eb188614fc7e50dcffcb1f553e3
BGE31_Zouheir <zacks81@hotmail.fr>
2017-08-22 14:11:37 +0200

    test

--- inc/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-    <ul>
-        <li><a href="about.php">à propos</a></li>
-        <li><a href="mentionslegales.php">mentions légales</a></li>
-        <li>2017 copyright</li>
-    </ul>
-</footer>
